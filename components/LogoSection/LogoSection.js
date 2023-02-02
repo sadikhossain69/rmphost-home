309373b5aa9a333c41c8bc2f2a1c9d6e5cc92378
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-02 12:48:42 +0600

    swipper done logo section

--- components/LogoSection/LogoSection.js
+++ components/LogoSection/LogoSection.js
@@ -4,6 +4,11 @@ import Logo3 from '@/assets/logos_3.png';
 import Logo4 from '@/assets/logos_4.png';
 import Logo5 from '@/assets/logos_5.png';
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+// import 'swiper/css/scrollbar';
 
 const LogoSection = () => {
     return (
@@ -11,51 +16,69 @@ const LogoSection = () => {
             <section id="logo_part">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg col-6 col-md-auto">
-                            <div className="logos logos_mar">
-                                <Image
-                                    src={Logo5}
-                                    alt="Picture of the Logo5"
-                                    width={150}
-                                />
-                            </div>
-                        </div>
-                        <div className="col-lg col-6 col-md-auto">
-                            <div className="logos logos_mar">
-                                <Image
-                                    src={Logo2}
-                                    alt="Picture of the Logo2"
-                                    width={150}
-                                />
-                            </div>
-                        </div>
-                        <div className="col-lg col-6 col-md-auto">
-                            <div className="logos">
-                                <Image
-                                    src={Logo3}
-                                    alt="Picture of the Logo3"
-                                    width={150}
-                                />
-                            </div>
-                        </div>
-                        <div className="col-lg col-6 col-md-auto">
-                            <div className="logos">
-                                <Image
-                                    src={Logo4}
-                                    alt="Picture of the Logo4"
-                                    width={150}
-                                />
-                            </div>
-                        </div>
-                        <div className="col-lg col-6 col-md-auto">
-                            <div className="logos marsis">
-                                <Image
-                                    src={Logo1}
-                                    alt="Picture of the Logo1"
-                                    width={150}
-                                />
-                            </div>
-                        </div>
+                        <Swiper
+                            spaceBetween={100}
+                            slidesPerView={3}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}
+
+                        >
+                            <SwiperSlide>
+                                <div className="col-lg col-6 col-md-auto">
+                                    <div className="logos logos_mar">
+                                        <Image
+                                            src={Logo5}
+                                            alt="Picture of the Logo5"
+                                            width={150}
+                                        />
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="col-lg col-6 col-md-auto">
+                                    <div className="logos logos_mar">
+                                        <Image
+                                            src={Logo2}
+                                            alt="Picture of the Logo2"
+                                            width={150}
+                                        />
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="col-lg col-6 col-md-auto">
+                                    <div className="logos">
+                                        <Image
+                                            src={Logo3}
+                                            alt="Picture of the Logo3"
+                                            width={150}
+                                        />
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="col-lg col-6 col-md-auto">
+                                    <div className="logos">
+                                        <Image
+                                            src={Logo4}
+                                            alt="Picture of the Logo4"
+                                            width={150}
+                                        />
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="col-lg col-6 col-md-auto">
+                                    <div className="logos marsis">
+                                        <Image
+                                            src={Logo1}
+                                            alt="Picture of the Logo1"
+                                            width={150}
+                                        />
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
             </section>
