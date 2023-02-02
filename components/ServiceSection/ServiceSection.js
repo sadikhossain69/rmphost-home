9e4bf9aae49d465112ef822b83e219999500367b
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-02 17:40:42 +0600

    aos installed in animate in service section

--- components/ServiceSection/ServiceSection.js
+++ components/ServiceSection/ServiceSection.js
@@ -4,77 +4,78 @@ import service3 from '@/assets/service3.png';
 import service_4 from '@/assets/service_4.png';
 import Image from 'next/image';
 
+
 const ServiceSection = () => {
     return (
         <>
             <section id="service_part">
-                <div class="container">
-                    <div class="service_header">
+                <div className="container">
+                    <div className="service_header">
                         <h2>Our <span>Service</span></h2>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="service_1">
-                                <div class="service_img">
+                    <div className="row">
+                        <div data-aos="zoom-in" className="col-lg-3 col-sm-6">
+                            <div className="service_1">
+                                <div className="service_img">
                                     <Image
                                         src={service_4}
                                         alt="service_4"
                                         width={220}
                                     />
                                 </div>
-                                <div class="service_content">
+                                <div className="service_content">
                                     <h3>Web Development</h3>
                                     <p>Get awe-inspiring business-ready web applications & features into production on-time.</p>
-                                    <p class="laravel">#Laravel  #Express.js #wordpress #php #react js.</p>
+                                    <p className="laravel">#Laravel  #Express.js #wordpress #php #react js.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="service_1">
-                                <div class="service_img">
+                        <div data-aos="zoom-in" className="col-lg-3 col-sm-6">
+                            <div className="service_1">
+                                <div className="service_img">
                                     <Image
                                         src={service1}
                                         alt="service1"
                                         width={220}
                                     />
                                 </div>
-                                <div class="service_content">
+                                <div className="service_content">
                                     <h3>Graphic Design</h3>
                                     <p>Our expert designers can help your Brand Identity.</p>
-                                    <p class="laravel">#Photoshop #Illustrator #After effects #Adobe xd.</p>
+                                    <p className="laravel">#Photoshop #Illustrator #After effects #Adobe xd.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="service_1">
-                                <div class="service_img">
+                        <div data-aos="zoom-in" className="col-lg-3 col-sm-6">
+                            <div className="service_1">
+                                <div className="service_img">
                                     <Image
                                         src={service2}
                                         alt="service2"
                                         width={220}
                                     />
                                 </div>
-                                <div class="service_content">
+                                <div className="service_content">
                                     <h3>Seo Marketing</h3>
                                     <p>Dedicated to providing ROI-focused online marketing solution to increase your sales..</p>
-                                    <p class="laravel">#SEO #PPC #Content Marketing #Social media #Facebook Ads.</p>
+                                    <p className="laravel">#SEO #PPC #Content Marketing #Social media #Facebook Ads.</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-3 col-sm-6">
-                            <div class="service_1">
-                                <div class="service_img">
+                        <div data-aos="zoom-in" className="col-lg-3 col-sm-6">
+                            <div className="service_1">
+                                <div className="service_img">
                                     <Image
                                         src={service3}
                                         alt="service3"
                                         width={220}
                                     />
                                 </div>
-                                <div class="service_content">
+                                <div className="service_content">
                                     <h3>App Development</h3>
                                     <p>Managed Mobile App Development devoted to discovering & building your desired objectives.
                                     </p>
-                                    <p class="laravel"> #React Js  #Flutter  #Kotlin.</p>
+                                    <p className="laravel"> #React Js  #Flutter  #Kotlin.</p>
                                 </div>
                             </div>
                         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import "@/styles/style.css"
 import "@/styles/responsive.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 700,
+      // once: true,
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }
