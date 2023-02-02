f47d96fe518bea807f03d6eb7a2288fb85c4a193
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-02 12:32:22 +0600

    react icon installed and setup done

--- components/FooterSection/FooterSection.js
+++ components/FooterSection/FooterSection.js
@@ -1,5 +1,6 @@
 import companyLogo from '@/assets/company_logo.png';
 import Image from 'next/image';
+import { BsDribbble, BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs';
 
 const FooterSection = () => {
 
@@ -17,7 +18,7 @@ const FooterSection = () => {
                                     alt="company logo"
                                     width={100}
                                 />
-                                    <p> rpmhost is a Professional software and website agency Platform. Here we will provide you only interesting content, which you will like very much. Were dedicated to providing you the best of software and website agency.</p>
+                                <p> rpmhost is a Professional software and website agency Platform. Here we will provide you only interesting content, which you will like very much. Were dedicated to providing you the best of software and website agency.</p>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6 m-0 p-0">
@@ -69,21 +70,23 @@ const FooterSection = () => {
                             <div className="footer_4">
                                 <h6>Our Social Media</h6>
                                 <div className="social_iconss">
-                                    <a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/yourdevloper.io/"> <i className="fa-brands fa-facebook-f"></i></a>
-                                    <a href="https://www.linkedin.com/in/rpm-host-59a06a264/"><i className="fa-brands fa-linkedin-in"></i></a>
-                                    <a href="#"><i className="fa-brands fa-pinterest-p"></i></a>
-                                    <a href="https://www.instagram.com/p/CgedHFAhd8F/?igshid=YmMyMTA2M2Y%3D"> <i className="fa-brands fa-instagram"></i></a>
-                                    <a href="#"> <i className="fa-brands fa-dribbble"></i></a>
+                                    <a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/yourdevloper.io/"> <BsFacebook /> </a>
+                                    <a href="https://www.linkedin.com/in/rpm-host-59a06a264/"><BsLinkedin /></a>
+                                    {/* <a href="#"><i className="fa-brands fa-pinterest-p"></i></a> */}
+                                    <a href="https://www.instagram.com/p/CgedHFAhd8F/?igshid=YmMyMTA2M2Y%3D">
+                                        <BsInstagram />
+                                    </a>
+                                    <a href="#"> <i><BsDribbble /></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                 </div>
-                <hr className="hrs"/>
-                    <div className="footer_copy">
-                        <p>Copyright {presentYear} © Bustic | Powered by Rpmhost It Limited</p>
-                    </div>
+                <hr className="hrs" />
+                <div className="footer_copy">
+                    <p>Copyright {presentYear} © Bustic | Powered by Rpmhost It Limited</p>
+                </div>
             </section>
         </>
     );
--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
       </Head>
 
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"></Script>
+      <Script src="https://kit.fontawesome.com/a7d5affee1.js" crossOrigin="anonymous"></Script>
 
       {/* Hero Section Imported */}
       <HeroSection />
