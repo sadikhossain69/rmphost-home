b0a07b68e9abc08e3d71e79847323f09ef17d3ab
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-02 11:20:48 +0600

    logo section done

---
+++ components/LogoSection/LogoSection.js
@@ -0,0 +1,66 @@
+import Logo1 from '@/assets/logos_1.png';
+import Logo2 from '@/assets/logos_2.png';
+import Logo3 from '@/assets/logos_3.png';
+import Logo4 from '@/assets/logos_4.png';
+import Logo5 from '@/assets/logos_5.png';
+import Image from 'next/image';
+
+const LogoSection = () => {
+    return (
+        <>
+            <section id="logo_part">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg col-6 col-md-auto">
+                            <div class="logos logos_mar">
+                                <Image
+                                    src={Logo5}
+                                    alt="Picture of the Logo5"
+                                    width={150}
+                                />
+                            </div>
+                        </div>
+                        <div class="col-lg col-6 col-md-auto">
+                            <div class="logos logos_mar">
+                                <Image
+                                    src={Logo2}
+                                    alt="Picture of the Logo2"
+                                    width={150}
+                                />
+                            </div>
+                        </div>
+                        <div class="col-lg col-6 col-md-auto">
+                            <div class="logos">
+                                <Image
+                                    src={Logo3}
+                                    alt="Picture of the Logo3"
+                                    width={150}
+                                />
+                            </div>
+                        </div>
+                        <div class="col-lg col-6 col-md-auto">
+                            <div class="logos">
+                                <Image
+                                    src={Logo4}
+                                    alt="Picture of the Logo4"
+                                    width={150}
+                                />
+                            </div>
+                        </div>
+                        <div class="col-lg col-6 col-md-auto">
+                            <div class="logos marsis">
+                                <Image
+                                    src={Logo1}
+                                    alt="Picture of the Logo1"
+                                    width={150}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default LogoSection;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HeroSection from '@/components/HeroSection/HeroSection'
+import LogoSection from '@/components/LogoSection/LogoSection'
 import Head from 'next/head'
 import Script from 'next/script'
 
@@ -11,12 +12,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></Script>
+
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossOrigin="anonymous"></Script>
 
       {/* Hero Section Imported */}
       <HeroSection />
-
+      <LogoSection />
     </>
   )
 }
