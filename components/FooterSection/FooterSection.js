ba95168b3148edd738f7f41aeb6b86cca60cd935
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-02 12:08:26 +0600

    footer section edit

--- components/FooterSection/FooterSection.js
+++ components/FooterSection/FooterSection.js
@@ -1,49 +1,59 @@
+import companyLogo from '@/assets/company_logo.png';
+import Image from 'next/image';
+
 const FooterSection = () => {
+
+    const presentYear = new Date().getFullYear()
+
     return (
         <>
             <section id="footer_part">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3 col-md-6">
-                            <div class="footer_1">
-                                <img src="image/company_logo.png" alt="" srcset=""/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3 col-md-6">
+                            <div className="footer_1">
+                                <Image
+                                    src={companyLogo}
+                                    alt="company logo"
+                                    width={100}
+                                />
                                     <p> rpmhost is a Professional software and website agency Platform. Here we will provide you only interesting content, which you will like very much. Were dedicated to providing you the best of software and website agency.</p>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-md-6 m-0 p-0">
-                            <div class="footer_2">
+                        <div className="col-lg-4 col-md-6 m-0 p-0">
+                            <div className="footer_2">
                                 <h6>Contact</h6>
-                                <div class="footer_2_items d-flex">
-                                    <div class="footer_icons">
-                                        <i class="fa-solid fa-building"></i>
+                                <div className="footer_2_items d-flex">
+                                    <div className="footer_icons">
+                                        <i className="fa-solid fa-building"></i>
                                     </div>
                                     <span>Rpmhost Ltd</span>
                                 </div>
-                                <div class="footer_2_items d-flex">
-                                    <div class="footer_icons">
-                                        <i class="fa-solid fa-location-dot"></i>
+                                <div className="footer_2_items d-flex">
+                                    <div className="footer_icons">
+                                        <i className="fa-solid fa-location-dot"></i>
                                     </div>
                                     <span>FX2W+GF4, Raghunathpur, Ghoradhara, Jhargram, West Bengal 721507</span>
                                 </div>
-                                <div class="footer_2_items d-flex">
-                                    <div class="footer_icons">
-                                        <i class="fa-solid fa-phone"></i>
+                                <div className="footer_2_items d-flex">
+                                    <div className="footer_icons">
+                                        <i className="fa-solid fa-phone"></i>
                                     </div>
                                     <span>+91 70473 84947</span>
                                 </div>
-                                <div class="footer_2_items d-flex">
-                                    <div class="footer_icons">
-                                        <i class="fa-solid fa-message"></i>
+                                <div className="footer_2_items d-flex">
+                                    <div className="footer_icons">
+                                        <i className="fa-solid fa-message"></i>
                                     </div>
                                     <span>info@rpmhost.in
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-2 col-md-6 m-0 p-0">
-                            <div class="footer_3">
+                        <div className="col-lg-2 col-md-6 m-0 p-0">
+                            <div className="footer_3">
                                 <h6>Capabilities</h6>
-                                <div class="footer_3_items">
+                                <div className="footer_3_items">
                                     <ul>
                                         <li><a href="#">Web Development</a></li>
                                         <li><a href="#">Graphic Design</a></li>
@@ -55,24 +65,24 @@ const FooterSection = () => {
 
                             </div>
                         </div>
-                        <div class="col-lg-3 col-md-6 m-0 p-0">
-                            <div class="footer_4">
+                        <div className="col-lg-3 col-md-6 m-0 p-0">
+                            <div className="footer_4">
                                 <h6>Our Social Media</h6>
-                                <div class="social_iconss">
-                                    <a href="https://www.facebook.com/yourdevloper.io/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0"> <i class="fa-brands fa-facebook-f"></i></a>
-                                    <a href="https://www.linkedin.com/in/rpm-host-59a06a264/"><i class="fa-brands fa-linkedin-in"></i></a>
-                                    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
-                                    <a href="https://www.instagram.com/p/CgedHFAhd8F/?igshid=YmMyMTA2M2Y%3D"> <i class="fa-brands fa-instagram"></i></a>
-                                    <a href="#"> <i class="fa-brands fa-dribbble"></i></a>
+                                <div className="social_iconss">
+                                    <a target={"_blank"} rel="noreferrer" href="https://www.facebook.com/yourdevloper.io/"> <i className="fa-brands fa-facebook-f"></i></a>
+                                    <a href="https://www.linkedin.com/in/rpm-host-59a06a264/"><i className="fa-brands fa-linkedin-in"></i></a>
+                                    <a href="#"><i className="fa-brands fa-pinterest-p"></i></a>
+                                    <a href="https://www.instagram.com/p/CgedHFAhd8F/?igshid=YmMyMTA2M2Y%3D"> <i className="fa-brands fa-instagram"></i></a>
+                                    <a href="#"> <i className="fa-brands fa-dribbble"></i></a>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                 </div>
-                <hr class="hrs"/>
-                    <div class="footer_copy">
-                        <p>Copyright 2021 © Bustic | Powered by Rpmhost It Limited</p>
+                <hr className="hrs"/>
+                    <div className="footer_copy">
+                        <p>Copyright {presentYear} © Bustic | Powered by Rpmhost It Limited</p>
                     </div>
             </section>
         </>
