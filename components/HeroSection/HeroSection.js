80a4ac4b76f8b2c3b8083f3ca914f762e251ab65
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-07 23:02:43 +0600

    some design issue fixed

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -9,8 +9,8 @@ const HeroSection = () => {
                     <div className="row heros_row">
                         <div data-aos="fade-right" className="col-lg-7 ">
                             <div className="hero_left">
-                                <h1>We Will Help Your Business <br />
-                                    <span>Grow 2x</span> Times Using Our Best Technologies</h1>
+                                <h1>We Will Help Your Business 
+                                    <span> Grow 2x</span> Times Using Our Best Technologies</h1>
 
                                 <p>Welcome To rpmhost
                                     rpmhost is a Professional software and website agency Platform. Here we will provide you only interesting content, which you will like very much. Were dedicated to providing you the best of software and website agency. </p>
