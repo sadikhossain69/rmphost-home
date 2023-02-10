1c4544f041248d5140cb6b459e2ee6c0a20a79cb
sadikhossain69 <sadikhossain433@gmail.com>
2023-02-10 21:54:50 +0600

    slider issue solved

--- components/LogoSection/LogoSection.js
+++ components/LogoSection/LogoSection.js
@@ -4,153 +4,63 @@ import Logo3 from '@/assets/logos_3.png';
 import Logo4 from '@/assets/logos_4.png';
 import Logo5 from '@/assets/logos_5.png';
 import Image from 'next/image';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-// import 'swiper/css/scrollbar';
+import { Carousel, ScrollingCarousel } from '@trendyol-js/react-carousel';
 
 const LogoSection = () => {
     return (
         <>
             <section id="logo_part">
                 <div className="container">
-                    <div className="row">
-                        <Swiper
-                            loop={true}
-                            spaceBetween={100}
-                            breakpoints={{
-                                // when window width is >= 640px
-                                500: {
-                                  width: 500,
-                                  slidesPerView: 2,
-                                },
-                                // when window width is >= 768px
-                                768: {
-                                  width: 768,
-                                  slidesPerView: 3,
-                                },
-                                992: {
-                                  width: 992,
-                                  slidesPerView: 4,
-                                },
-                              }}
-                            onSlideChange={() => console.log('slide change')}
-                            onSwiper={(swiper) => console.log(swiper)}
+                        <div className="row">
+                    <Carousel show={4} slide={3} swiping={true} infinite={true} transition={0.2} >
 
-                        >
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos logos_mar">
-                                        <Image
-                                            src={Logo5}
-                                            alt="Picture of the Logo5"
-                                            width={150}
-                                        />
-                                    </div>
+                            <div className="col-lg col-12 col-md-auto">
+                                <div className="logos logos_mar">
+                                    <Image
+                                        src={Logo5}
+                                        alt="Picture of the Logo5"
+                                        width={150}
+                                    />
                                 </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos logos_mar">
-                                        <Image
-                                            src={Logo2}
-                                            alt="Picture of the Logo2"
-                                            width={150}
-                                        />
-                                    </div>
+                            </div>
+                            <div className="col-lg col-12 col-md-auto">
+                                <div className="logos logos_mar">
+                                    <Image
+                                        src={Logo2}
+                                        alt="Picture of the Logo2"
+                                        width={150}
+                                    />
                                 </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos">
-                                        <Image
-                                            src={Logo3}
-                                            alt="Picture of the Logo3"
-                                            width={150}
-                                        />
-                                    </div>
+                            </div>
+                            <div className="col-lg col-12 col-md-auto">
+                                <div className="logos">
+                                    <Image
+                                        src={Logo3}
+                                        alt="Picture of the Logo3"
+                                        width={150}
+                                    />
                                 </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos">
-                                        <Image
-                                            src={Logo4}
-                                            alt="Picture of the Logo4"
-                                            width={150}
-                                        />
-                                    </div>
+                            </div>
+                            <div className="col-lg col-12 col-md-auto">
+                                <div className="logos">
+                                    <Image
+                                        src={Logo4}
+                                        alt="Picture of the Logo4"
+                                        width={150}
+                                    />
                                 </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos marsis">
-                                        <Image
-                                            src={Logo1}
-                                            alt="Picture of the Logo1"
-                                            width={150}
-                                        />
-                                    </div>
+                            </div>
+                            <div className="col-lg col-12 col-md-auto">
+                                <div className="logos marsis">
+                                    <Image
+                                        src={Logo1}
+                                        alt="Picture of the Logo1"
+                                        width={150}
+                                    />
                                 </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos logos_mar">
-                                        <Image
-                                            src={Logo5}
-                                            alt="Picture of the Logo5"
-                                            width={150}
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos logos_mar">
-                                        <Image
-                                            src={Logo2}
-                                            alt="Picture of the Logo2"
-                                            width={150}
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos">
-                                        <Image
-                                            src={Logo3}
-                                            alt="Picture of the Logo3"
-                                            width={150}
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos">
-                                        <Image
-                                            src={Logo4}
-                                            alt="Picture of the Logo4"
-                                            width={150}
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className="col-lg col-12 col-md-auto">
-                                    <div className="logos marsis">
-                                        <Image
-                                            src={Logo1}
-                                            alt="Picture of the Logo1"
-                                            width={150}
-                                        />
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        </Swiper>
-                    </div>
+                            </div>
+                    </Carousel>
+                        </div>
                 </div>
             </section>
         </>
